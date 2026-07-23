edebc9e064fd9bdcdcea35f7e28ea72aa40358d8
James Foucar <jgfouca@sandia.gov>
2026-07-23 14:31:41 -0600

    Add support for kokkos and KK

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
+#include <Kokkos_Core.hpp>
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  Kokkos::initialize(argc, argv);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  int result = RUN_ALL_TESTS();
+
+  Kokkos::finalize();
+
+  return result;
 }
